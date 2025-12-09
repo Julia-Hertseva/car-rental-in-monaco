a27767cb8328d869aeb94f6c7477c7ae945c5cef
Julia-Hertseva <gerceva@gmail.com>
2025-12-09 21:25:02 +0200

    refactor: restructure SCSS files (BEM blocks, components, utilities)

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,17 @@ const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const concatCss = require('gulp-concat-css');
 function defaultTask() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concatCss("style.css"))
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist'));
+    return gulp
+      .src("./src/styles/main.scss")
+      .pipe(sass().on("error", sass.logError))
+      .pipe(concatCss("style.css"))
+      .pipe(cssmin())
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(gulp.dest("./dist"));
 }
 
 exports.default = defaultTask;
 
 exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', gulp.series ('default'));
+    gulp.watch('./src/styles/**/*.scss', gulp.series ('default'));
 };
